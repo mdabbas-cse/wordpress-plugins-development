d87caca2c5586e63f4a073e2151221ba4b1f893b
mdabbas-cse <gmabbas44@gmail.com>
2023-08-13 15:45:02 +0600

    create: wp header in plugin index file

---
+++ aquila-features.php
@@ -0,0 +1,17 @@
+<!-- https://downloadlynet.ir/2023/15/93532/03/creating-wordpress-plugins-the-right-way/11/?#/93532-udemy-112323082813.html -->
+<?php
+
+/*
+ * Plugin Name:       Aquila Features
+ * Plugin URI:        https://aquila.com/features
+ * Description:       Adds a custom post type to display features by gotenbueg blog.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Md. Abbas Uddin
+ * Author URI:        https:github.com/mdabbas-cse
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       aquila-features
+ * Domain Path:       /languages
+ */
